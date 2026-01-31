c0aeea4b2dceec9ac36f2ec6564e492133bbab92
Jeremy de León <jeremydeleon@Mac-mini-de-Jeremy.local>
2026-01-31 16:36:53 -0600

    Pruebas manejo de DOM

---
+++ MetalCode/01-fundamentos/08-DOM/script.js
@@ -0,0 +1,51 @@
+const parrafo = document.getElementById("parrafo");
+parrafo.innerHTML = "Hola Mundo desde el DOM";
+
+const buttonSubmit = document.getElementById("btn-submit");
+buttonSubmit.textContent = "Enviar Datos";
+buttonSubmit.style.backgroundColor = "blue";
+buttonSubmit.style.color = "white";
+buttonSubmit.style.padding = "10px 20px";
+buttonSubmit.style.borderRadius = "5px";
+buttonSubmit.style.cursor = "pointer";
+
+const nuevoParrofo = document.createElement("p");
+nuevoParrofo.textContent = "Este es un nuevo párrafo creado con JavaScript.";
+document.body.appendChild(nuevoParrofo);
+
+const nuevoElementoLista = document.createElement("li");
+nuevoElementoLista.textContent = "Elemento de lista añadido dinámicamente";
+document.querySelector("ul").appendChild(nuevoElementoLista);
+
+// Debe clonarse porque un elemento solo puede estar en un lugar del DOM
+const ElementoAmbasListas = document.createElement("li");
+ElementoAmbasListas.textContent = "Elemento añadido a ambas listas";
+const listas = document.querySelectorAll("ul");
+// Recorremos las listas y añadimos el elemento clonado a cada una
+for (const lista of listas) {
+    lista.appendChild(ElementoAmbasListas.cloneNode(true));
+}
+
+// Sintaxis de eventos en javascript
+buttonSubmit.addEventListener("click", function(){
+    buttonSubmit.style.backgroundColor = "green";
+    buttonSubmit.textContent = "Clickeado";
+});
+
+const buttonJS = document.createElement("button");
+buttonJS.textContent = "Botón creado con JS";
+document.body.appendChild(buttonJS);
+
+buttonJS.addEventListener("mouseover", function(){
+    buttonJS.textContent = "Mouse Over";
+});
+
+// Regresa a su estado original
+buttonJS.addEventListener("mouseout", function(){
+    buttonJS.textContent = "Botón creado con JS";
+    buttonJS.style.backgroundColor = "";
+} );
+
+buttonJS.addEventListener("dblclick", function(){
+    buttonJS.style.backgroundColor = "red";
+});
