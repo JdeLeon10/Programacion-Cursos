5f9579b07e53322fd38632150a8454ac5d96902b
Jeremy <jere.deleon8520@gmail.com>
2026-03-22 19:38:34 -0600

    No longer need files

--- react/freecodecamp/01-react-basics/src/Components/FooterPage1.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const FooterPage1 = () => {
-  return (
-    <footer>
-      <small>© 2024 Jeremy Deleon. All rights are reserved.</small>
-    </footer>
-  );
-};
