91de259fdae71059f430530767fa532c83fc4f84
Jeremy De Leon <jere.deleon8520@gmail.com>
2026-03-24 15:48:19 -0600

    Digital Clock example

--- react/bases/00-react-bases/src/DigitalClockApp.jsx
+++ react/bases/00-react-bases/src/DigitalClockApp.jsx
@@ -1,10 +1,23 @@
 import "./DigitalClockApp.css";
-import React from "react";
+import { useState, useEffect } from "react";
 
 export const DigitalClockApp = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000); // Cada segundo se actualiza la hora
+
+    return () => clearInterval(intervalId); // Limpiar el intervalo al desmontar el componente
+  }, []); // El efecto se ejecutará solo una vez después del primer renderizado
+
   return (
     <>
-      <h1>Digital Clock App</h1>
+      <h1 className="clock-title">Digital Clock App</h1>
+      <div className="clock">
+        <span>{time.toLocaleTimeString()}</span>
+      </div>
     </>
   );
 };
