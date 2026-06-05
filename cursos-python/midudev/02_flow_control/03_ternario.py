c0b2bbf52768da6dd8defdcf50a35528e1386e83
Jeremy De Leon <jere.deleon8520@gmail.com>
2026-06-04 20:39:29 -0600

    Eliminacion de archivos

--- cursos-python/midudev/02_flow_control/03_ternario.py
+++
@@ -1,8 +0,0 @@
-# Forma concisa de un if-else en una linea de codigo
-# 1. codigo si cumple la condicion
-# if conficion
-# 2. codigo si no cumple la condicion
-
-edad = 19
-mensaje = "Es mayor de edad" if edad >= 18 else "Es menor de edad"
-print(mensaje)
